10cf5f0d458e5e3e804ecc9a1d2eae882e714399
Elfi_Y <elfiyang16@gmail.com>
2021-08-08 21:06:32 +0100

    fix

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -82,7 +82,7 @@ locals {
 data "archive_file" "lambda_medium_contentful" {
   type = "zip"
 
-  source_dir  = "${path.module}/../.dist"
+  source_dir  = "${path.module}/../src"
   output_path = "${path.module}/../.dist/src.zip"
 }
 
