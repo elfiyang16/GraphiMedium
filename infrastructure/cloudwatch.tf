3fc113e4dd52641f0245a46c1fef52722c981dcb
Elfi_Y <elfiyang16@gmail.com>
2021-08-08 18:57:38 +0100

    fix: cron syntax for aws

--- infrastructure/cloudwatch.tf
+++ infrastructure/cloudwatch.tf
@@ -3,7 +3,7 @@ resource "aws_cloudwatch_event_rule" "every_week" {
   description = "Fires every week"
   #   weekly Sunday at 9:30 
   #   schedule_expression = "cron(30 9 * * 0)"
-  schedule_expression = "cron(57 17 * * 0)"
+  schedule_expression = "cron(7 18 * * ? SUN)"
 }
 
 resource "aws_cloudwatch_event_target" "lambda_medium_contentful" {
