7d1ad46b5a4bad99fda81e880e8fe44a722dc5a1
Elfi_Y <elfiyang16@gmail.com>
2021-08-26 20:09:05 +0100

    fix: tf s3

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,47 +1,47 @@
 //TODO:
 # cloudwatch event time trigger as variable
 
-resource "random_id" "lambda_get_post_bucket_name" {
-  byte_length = 6
+resource "random_pet" "lambda_get_post_bucket_name" {
+  length = 4
   prefix = "medium-contentful-get-post"
 }
 
-resource "random_id" "lambda_publish_post_bucket_name" {
-  byte_length = 6
+resource "random_pet" "lambda_publish_post_bucket_name" {
+  length = 4
   prefix = "medium-contentful-publish-post"
 }
 
-resource "random_id" "lambda_node_modules_layer_bucket_name" {
-  byte_length = 6
+resource "random_pet" "lambda_node_modules_layer_bucket_name" {
+  length = 4
   prefix = "medium-contentful-node-modules-layer"
 }
 
-resource "random_id" "lambda_shared_layer_bucket_name" {
-  byte_length = 6
+resource "random_pet" "lambda_shared_layer_bucket_name" {
+  length = 4
   prefix = "medium-contentful-shared-layer"
 }
 
 
 resource "aws_s3_bucket" "lambda_get_post_bucket" {
-  bucket        = random_id.lambda_get_post_bucket_name.id
+  bucket        = random_pet.lambda_get_post_bucket_name.id
   acl           = "private"
   force_destroy = true
 }
 
 resource "aws_s3_bucket" "lambda_publish_post_bucket" {
-  bucket        = random_id.lambda_publish_post_bucket_name.id
+  bucket        = random_pet.lambda_publish_post_bucket_name.id
   acl           = "private"
   force_destroy = true
 }
 
 resource "aws_s3_bucket" "lambda_node_modules_layer_bucket" {
-  bucket        = random_id.lambda_node_modules_layer_bucket_name.id
+  bucket        = random_pet.lambda_node_modules_layer_bucket_name.id
   acl           = "private"
   force_destroy = true
 }
 
 resource "aws_s3_bucket" "lambda_shared_layer_bucket" {
-  bucket        = random_id.lambda_shared_layer_bucket_name.id
+  bucket        = random_pet.lambda_shared_layer_bucket_name.id
   acl           = "private"
   force_destroy = true
 }
