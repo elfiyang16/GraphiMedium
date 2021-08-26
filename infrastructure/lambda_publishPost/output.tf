7c48de892aef5e7f5afc0ddebd47f7ebbba277b9
Elfi_Y <elfiyang16@gmail.com>
2021-08-26 18:31:18 +0100

    fix: typo

--- infrastructure/lambda_publishPost/output.tf
+++ infrastructure/lambda_publishPost/output.tf
@@ -2,11 +2,10 @@
 
 output "function_name" {
   description = "Name of the Lambda function."
-  value = aws_lambda_function.lambda_publish_post.function_name
+  value       = aws_lambda_function.lambda_publish_post.function_name
 }
 
 output "sqs_publish_post_queue_arn" {
-  type    = string
   description = "The SQS Arn for publish the posts."
-  value = aws_sqs_queue.sqs_publish_post.arn
+  value       = aws_sqs_queue.sqs_publish_post.arn
 }
